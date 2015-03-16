6ecb6e0fe29070c1cfc43e267e866ba1e3fb7b39
Jakub Wlodarczyk <wlodarczykj1027@gmail.com>
2015-03-16 14:18:20 -0500

    Added ParseMusicQueue

---
+++ app/src/main/java/com/example/lucasrosario/extensionchord/ParseMusicQueue.java
@@ -0,0 +1,21 @@
+package com.example.lucasrosario.extensionchord;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Jakub on 3/16/2015.
+ */
+@ParseClassName("ParseMusicQueue")
+public class ParseMusicQueue extends ParseObject {
+    List<Integer> trackIDs = new ArrayList<Integer>();
+
+    public void addTrackToQueue(int trackID){ add("tracks", trackID);}
+
+    public List<Integer> getTrackList(){ return getList("tracks");}
+
+    public Integer getCurrentTrack(){ return (int)getList("tracks").get(0); }
+}
