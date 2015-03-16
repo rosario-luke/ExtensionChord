f69459c4f29bce2f5851d13b8fa7015985a3d8cb
Brett Hohler <brett.hohler@gmail.com>
2015-03-15 21:05:19 -0500

    Made RoomUser extend ParseObject and added methods for interacting with the new data it stores

--- app/src/main/java/com/example/lucasrosario/extensionchord/RoomUser.java
+++ app/src/main/java/com/example/lucasrosario/extensionchord/RoomUser.java
@@ -1,9 +1,34 @@
 package com.example.lucasrosario.extensionchord;
 
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
 /**
  * Created by Brett on 3/3/2015.
  */
-public class RoomUser {
+@ParseClassName("RoomUser")
+public class RoomUser extends ParseObject {
+    public String getCurrentRoom() {
+        return getString("currentRoom");
+    }
+
+    public void setCurrentRoom(String roomName) {
+        put("currentRoom", roomName);
+    }
+
+    public String getUsername() {
+        return getString("username");
+    }
+
+    public void setUsername(String username) {
+        put("username", username);
+    }
+
+    public static ParseQuery<RoomUser> getQuery() {
+        return ParseQuery.getQuery(RoomUser.class);
+    }
 
     /**
      * Allows a user to join a room.
