fe6f56b24fb22a7e655c1d1f905ebe5ca5044fd7
pritz2 <pritzluke@gmail.com>
2015-04-07 18:56:28 -0500

    Added current song display item

---
+++ app/src/main/java/com/example/lucasrosario/extensionchord/CurrentSongDisplayItem.java
@@ -0,0 +1,87 @@
+package com.example.lucasrosario.extensionchord;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.media.MediaPlayer;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by lucaspritz on 4/7/15.
+ */
+public class CurrentSongDisplayItem extends LinearLayout{
+
+    private ParseTrack track;
+
+    public CurrentSongDisplayItem(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        TypedArray a = context.obtainStyledAttributes(attrs,
+                R.styleable.ViewTrackDisplayItem, 0, 0);
+        String track = a.getString(R.styleable.ViewTrackDisplayItem_view_track_name);
+        String artist = a.getString(R.styleable.ViewTrackDisplayItem_view_artist_name);
+        String album = a.getString(R.styleable.ViewTrackDisplayItem_view_album_name);
+        a.recycle();
+
+        setOrientation(LinearLayout.HORIZONTAL);
+        setGravity(Gravity.CENTER_VERTICAL);
+
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.view_track_display_item, this, true);
+
+        TextView track_view = (TextView)this.findViewById(R.id.track_name);
+        track_view.setText(track);
+
+        TextView artist_view = (TextView)this.findViewById(R.id.artist_name);
+        artist_view.setText(artist);
+
+        TextView album_view = (TextView)this.findViewById(R.id.album_name);
+        album_view.setText(album);
+    }
+
+    public CurrentSongDisplayItem(Context context, ParseTrack t, int count) {
+        super(context);
+
+        this.track = t;
+        setOrientation(LinearLayout.HORIZONTAL);
+        setGravity(Gravity.CENTER_VERTICAL);
+
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.current_song, this, true);
+
+        TextView track_view = (TextView)this.findViewById(R.id.track_name);
+        track_view.setText(track.getTrackName());
+
+        TextView artist_view = (TextView)this.findViewById(R.id.artist_name);
+        artist_view.setText(track.getTrackArtist());
+
+        TextView album_view = (TextView)this.findViewById(R.id.album_name);
+        album_view.setText(track.getTrackAlbum());
+
+        TextView track_number = (TextView)this.findViewById(R.id.track_number);
+        track_number.setText(count + ".)");
+    }
+
+    public String getTrackName(){
+        return ((TextView)this.findViewById(R.id.track_name)).getText().toString();
+    }
+
+    public ParseTrack getTrack(){ return track;}
+
+    public void setPlayListener(OnClickListener listener){
+        Button play_button = (Button)this.findViewById(R.id.play_button);
+        play_button.setOnClickListener(listener);
+    }
+
+    public void setPauseListener(OnClickListener listener){
+        Button pause_button = (Button)this.findViewById(R.id.pause_button);
+        pause_button.setOnClickListener(listener);
+    }
+
+}
