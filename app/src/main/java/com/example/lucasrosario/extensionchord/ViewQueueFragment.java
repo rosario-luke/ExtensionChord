c288e88d36fd774fd8df9aad8883f37b64ab898d
lucas rosario <lrosari2@illinois.edu>
2015-04-08 16:00:54 -0500

    Deleting TODO's

--- app/src/main/java/com/example/lucasrosario/extensionchord/ViewQueueFragment.java
+++ app/src/main/java/com/example/lucasrosario/extensionchord/ViewQueueFragment.java
@@ -24,7 +24,7 @@ import java.util.List;
  */
 public class ViewQueueFragment extends Fragment {
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
 
@@ -119,7 +119,7 @@ public class ViewQueueFragment extends Fragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
+
         public void onFragmentInteraction(Uri uri);
     }
 
