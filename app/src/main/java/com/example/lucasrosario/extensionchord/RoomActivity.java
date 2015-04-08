6733fd268e675b5d8fae63997d45c8b68687d882
lucas rosario <lrosari2@illinois.edu>
2015-04-08 12:30:38 -0500

    NavigationDrawer closes now, Fixed Refreshing Music Queue when you didn't add the music. Note: Have to make that asynchronous

--- app/src/main/java/com/example/lucasrosario/extensionchord/RoomActivity.java
+++ app/src/main/java/com/example/lucasrosario/extensionchord/RoomActivity.java
@@ -165,6 +165,7 @@ public class RoomActivity extends Activity {
                         finish();
                         break;
                 }
+                mDrawerLayout.closeDrawers();
             }
         });
     }
--- app/src/main/java/com/example/lucasrosario/extensionchord/ViewQueueFragment.java
+++ app/src/main/java/com/example/lucasrosario/extensionchord/ViewQueueFragment.java
@@ -11,6 +11,8 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.parse.ParseException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -64,8 +66,17 @@ public class ViewQueueFragment extends Fragment {
 
     public void buildTrackList(){
         String roomName = ((RoomActivity)getActivity()).getRoomName();
-        ParseRoom currRoom = RoomManager.getParseRoom(roomName);
-        ParseMusicQueue currQueue = currRoom.getParseMusicQueue();
+        ParseRoom currRoom;
+        ParseMusicQueue currQueue;
+        try{
+            // TODO: Change this to be asynchronous
+            currRoom = RoomManager.getParseRoom(roomName).fetchIfNeeded();
+            currQueue = currRoom.getParseMusicQueue().fetchIfNeeded();
+        } catch(ParseException e){
+            Toast.makeText(getActivity(), "Error occured while fetching room info", Toast.LENGTH_SHORT);
+            return;
+        }
+
 
         addTracks(currQueue.getTrackList());
     }
