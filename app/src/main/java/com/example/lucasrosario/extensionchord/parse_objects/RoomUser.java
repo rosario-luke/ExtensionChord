03242afef4bb616c12a9841629dfab70deb97026
Brett Hohler <brett.hohler@gmail.com>
2015-04-30 14:24:40 -0500

    Adding Javadoc comments

--- app/src/main/java/com/example/lucasrosario/extensionchord/parse_objects/RoomUser.java
+++ app/src/main/java/com/example/lucasrosario/extensionchord/parse_objects/RoomUser.java
@@ -10,28 +10,61 @@ import com.parse.ParseQuery;
  */
 @ParseClassName("RoomUser")
 public class RoomUser extends ParseObject {
+    /**
+     * Gets the current user's room name
+     *
+     * @return The room name
+     */
     public String getCurrentRoom() {
         return getString("currentRoom");
     }
 
+    /**
+     * Sets the current user's room name
+     *
+     * @param roomName The room name
+     */
     public void setCurrentRoom(String roomName) {
         put("currentRoom", roomName);
     }
 
+    /**
+     * Returns whether or not this user is an admin
+     *
+     * @return True if the user is an admin, otherwise false
+     */
     public boolean isAdmin() { return getBoolean("isAdmin"); }
 
+    /**
+     * Sets whether or not the user is an admin
+     *
+     * @param isAdmin True if making them an admin, otherwise false
+     */
     public void setAdmin(boolean isAdmin) {
         put("isAdmin", isAdmin);
     }
 
+    /**
+     * Gets the username
+     * @return The username
+     */
     public String getUsername() {
         return getString("username");
     }
 
+    /**
+     * Sets the username
+     * @param username The username
+     */
     public void setUsername(String username) {
         put("username", username);
     }
 
+    /**
+     * Gets a query allowing you to query RoomUsers
+     *
+     * @return A RoomUser query
+     */
     public static ParseQuery<RoomUser> getQuery() {
         return ParseQuery.getQuery(RoomUser.class);
     }
